0e792bd74f4016c44395a76900c116aef0640de9
iaw47999659 <iaw47999659@h01.informatica.escoladeltreball.org>
2015-10-07 17:31:15 +0200

    ejercicios

--- ejercicios/src/Conjunts3.java
+++ ejercicios/src/Conjunts3.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.TreeSet;
 
 public class Conjunts3 {
@@ -8,49 +9,29 @@ public class Conjunts3 {
 		ComparatorCars2 comparator2 = new ComparatorCars2();
 		
 		TreeSet<Car> carSet1 = new TreeSet<Car>(comparator1);
-
-		carSet1.add(new Car("Alfa Romeo", "Giulia", 2900, 6));
-		carSet1.add(new Car("Dacia", "Sandero", 1200, 4));
-		carSet1.add(new Car("Ford", "Focus", 2000, 4));
-		carSet1.add(new Car("Opel", "Insignia", 2200, 4));
-		carSet1.add(new Car("Seat", "Ibiza", 1600, 4));
-		carSet1.add(new Car("Hyundai", "Atos", 1500, 3));
-		
 		TreeSet<Car> carSet2 = new TreeSet<Car>(comparator1);
-
-		carSet2.add(new Car("Alfa Romeo", "Giulia", 2900, 6));
-		carSet2.add(new Car("Dacia", "Sandero", 1200, 4));
-		carSet2.add(new Car("Ford", "Focus", 2000, 4));
-		carSet2.add(new Car("Opel", "Insignia", 2200, 4));
-		carSet2.add(new Car("Seat", "Ibiza", 1600, 4));
-		carSet2.add(new Car("Hyundai", "Atos", 1500, 3));
-
 		TreeSet<Car> carSet3 = new TreeSet<Car>(comparator2);
-
-		carSet3.add(new Car("Alfa Romeo", "Giulia", 2900, 6));
-		carSet3.add(new Car("Dacia", "Sandero", 1200, 4));
-		carSet3.add(new Car("Ford", "Focus", 2000, 4));
-		carSet3.add(new Car("Opel", "Insignia", 2200, 4));
-		carSet3.add(new Car("Seat", "Ibiza", 1600, 4));
-		carSet3.add(new Car("Hyundai", "Atos", 1500, 3));
-
 		TreeSet<Car> carSet4 = new TreeSet<Car>(comparator2);
 
-		carSet4.add(new Car("Alfa Romeo", "Giulia", 2900, 6));
-		carSet4.add(new Car("Dacia", "Sandero", 1200, 4));
-		carSet4.add(new Car("Ford", "Focus", 2000, 4));
-		carSet4.add(new Car("Opel", "Insignia", 2200, 4));
-		carSet4.add(new Car("Seat", "Ibiza", 1600, 4));
-		carSet4.add(new Car("Hyundai", "Atos", 1500, 3));
+		LinkedList<Car> carList = new LinkedList<Car>();
+		carList.add(new Car("Alfa Romeo", "Giulia", 2900, 6));
+		carList.add(new Car("Dacia", "Sandero", 1200, 4));
+		carList.add(new Car("Ford", "Focus", 2000, 4));
+		carList.add(new Car("Opel", "Insignia", 2200, 4));
+		carList.add(new Car("Seat", "Ibiza", 1600, 4));
+		carList.add(new Car("Hyundai", "Atos", 1500, 3));
+		
+		carSet1.addAll(carList);
+		carSet2.addAll(carList);
+		carSet3.addAll(carList);
+		carSet4.addAll(carList);
 
 		System.out.println("LIFO CILINDRADA");
 
 		// shows the TreeSet sorted with the displacement by the lifo method
 
-		for (int i = 0; i <= carSet1.size(); i++) {
-			System.out.println(carSet1.last());
-			carSet1.remove(carSet1.last());
-			i = 0;
+		while (!carSet1.isEmpty()) {
+			System.out.println(carSet1.pollLast().toString());
 		}
 
 		System.out.println("------");
@@ -58,10 +39,8 @@ public class Conjunts3 {
 		System.out.println("LIFO POT. FISC");
 		
 		// shows the TreeSet sorted with the pot. fisc. by the lifo method
-		for (int i = 0; i <= carSet3.size(); i++) {
-			System.out.println(carSet3.last());
-			carSet3.remove(carSet3.last());
-			i = 0;
+		while (!carSet3.isEmpty()) {
+			System.out.println(carSet3.pollLast().toString());
 		}
 
 		System.out.println("------");
@@ -69,10 +48,8 @@ public class Conjunts3 {
 		System.out.println("FIFO CILINDRADA");
 
 		// shows the TreeSet sorted with the displacement by the fifo method
-		for (int i = 0; i <= carSet2.size(); i++) {
-			System.out.println(carSet2.first());
-			carSet2.remove(carSet2.first());
-			i = 0;
+		while (!carSet2.isEmpty()) {
+			System.out.println(carSet2.pollFirst().toString());
 		}
 
 		System.out.println("------");
@@ -80,10 +57,8 @@ public class Conjunts3 {
 		System.out.println("FIFO POT. FISC.");
 
 		// shows the TreeSet sorted with the pot. fisc. by the fifo method
-		for (int i = 0; i <= carSet4.size(); i++) {
-			System.out.println(carSet4.first());
-			carSet4.remove(carSet4.first());
-			i = 0;
+		while (!carSet4.isEmpty()) {
+			System.out.println(carSet4.pollFirst().toString());
 		}
 	}
 
--- ejercicios/src/Maps1.java
+++ ejercicios/src/Maps1.java
@@ -1,9 +1,30 @@
-
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Maps1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Map<String, Car> map = new TreeMap<String, Car>();
+		map.put(new String("1234 AAA"), new Car("Alfa Romeo", "Giulia", 2900, 6));
+		map.put(new String("2321 BBB"), new Car("Seat", "Ibiza", 1600, 3));
+		map.put(new String("8232 FFF"), new Car("Ford", "Focus", 2000, 4));
+		map.put(new String("3311 FFF"), new Car("Ford", "Mondeo", 2200, 4));
+		map.put(new String("9381 LLL"), new Car("Seat", "Leon", 1600, 4));
+		map.put(new String("B 9011 XC"), new Car("Hyundai", "Atos", 1500, 3));
+		map.put(new String("9234 BHG"), new Car("Hyundai", "Santa Fe", 3000, 6));
+		map.put(new String("9216 CKK"), new Car("Hyundai", "Accent", 1500, 4));
+		
+		/**boolean resultat = map.put(new String("9216 CKK"), new Car("Hyundai", "Accent", 1500, 4));
+		if (resultat) {
+			System.out.println("Hyundai afegit");
+		} else {
+			System.out.println("Hyundai no afegit");
+		}*/
+		
+		for(Map.Entry<String, Car> entry : map.entrySet()) {
+			  System.out.println(entry.getKey() + " => " + entry.getValue());
+		}
 
 	}
 
