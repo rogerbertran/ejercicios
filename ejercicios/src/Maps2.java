b280eb17b6587c13d9b91970ab3e6253e0849038
iaw47999659 <iaw47999659@h01.informatica.escoladeltreball.org>
2015-10-07 17:48:28 +0200

    ejercicios

--- ejercicios/src/Maps2.java
+++ ejercicios/src/Maps2.java
@@ -1,10 +1,16 @@
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Maps1 {
-
+public class Maps2 {
+	
+	public void eliminar() {
+		for (Car x : Map.map) {
+			
+		}
+	}
+	
 	public static void main(String[] args) {
-		// Add maps to the treeMap
+		// TODO Auto-generated method stub
 		Map<String, Car> map = new TreeMap<String, Car>();
 		map.put(new String("1234 AAA"), new Car("Alfa Romeo", "Giulia", 2900, 6));
 		map.put(new String("2321 BBB"), new Car("Seat", "Ibiza", 1600, 3));
@@ -14,18 +20,7 @@ public class Maps1 {
 		map.put(new String("B 9011 XC"), new Car("Hyundai", "Atos", 1500, 3));
 		map.put(new String("9234 BHG"), new Car("Hyundai", "Santa Fe", 3000, 6));
 		map.put(new String("9216 CKK"), new Car("Hyundai", "Accent", 1500, 4));
-		
-		/**boolean resultat = map.put(new String("9216 CKK"), new Car("Hyundai", "Accent", 1500, 4));
-		if (resultat) {
-			System.out.println("Hyundai afegit");
-		} else {
-			System.out.println("Hyundai no afegit");
-		}*/
-		
-		// Show them
-		for(Map.Entry<String, Car> entry : map.entrySet()) {
-			  System.out.println(entry.getKey() + " => " + entry.getValue());
-		}
+	
 
 	}
 
