489d5b1db6dce02b68ac4dddef2b6ba038bdc3de
Roger <roger.bertran.nebot@gmail.com>
2015-11-17 19:25:05 +0100

    a

--- ejercicios/src/NF10/Exercici2.java
+++ ejercicios/src/NF10/Exercici2.java
@@ -1,21 +1,30 @@
 package NF10;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Exercici2 {
+	public static void main(String[] args) throws IOException, FileNotFoundException {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		try {
-			FileInputStream bigfile = null;
-			FileOutputStream baseName = null;
-			int chunkSize = 15;
-			byte[] copia = new byte[chunkSize];
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+		FileInputStream in = new FileInputStream("bigOne.txt");
+		FileOutputStream out = new FileOutputStream("outagain0.txt");
+		int c = 0;
+		int i = 0;
+		int j = 1;
+		while ((c = in.read()) != -1) {
+			out.write(c);
+			i++;
+			if (i % 8 == 0) {
+				out.close();
+				out = new FileOutputStream("outagain" + j + ".txt");
+				j++;
+			}
 		}
+		out.close();
+		in.close();
+
 	}
+
 }
